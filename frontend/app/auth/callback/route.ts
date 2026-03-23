c0f9dd9e038714a2ab9c9a36faef755ae195f7a6
Nexon <nexonicindustries@gmail.com>
2026-03-24 00:06:54 +0300

    updates

--- frontend/app/auth/callback/route.ts
+++ frontend/app/auth/callback/route.ts
@@ -1,30 +1,44 @@
 import { NextResponse } from 'next/server'
-// The client you created in Step 2
 import { createClient } from '@/utils/supabase/server'
+import { type EmailOtpType } from '@supabase/supabase-js'
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
-  // if "next" is in search params, use it as the redirection URL after confirmation
+  const tokenHash = searchParams.get('token_hash')
+  const type = searchParams.get('type') as EmailOtpType | null
   const next = searchParams.get('next') ?? '/onboarding'
 
+  const supabase = await createClient()
+
   if (code) {
-    const supabase = await createClient()
     const { error } = await supabase.auth.exchangeCodeForSession(code)
     if (!error) {
-      const forwardedHost = request.headers.get('x-forwarded-host') // i.e. vercel.app
-      const isLocalEnv = process.env.NODE_ENV === 'development'
-      if (isLocalEnv) {
-        // we can be sure that there is no proxy in between in local dev
-        return NextResponse.redirect(`${origin}${next}`)
-      } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`)
-      } else {
-        return NextResponse.redirect(`${origin}${next}`)
-      }
+      return safeRedirect(request, origin, next)
+    }
+  } else if (tokenHash && type) {
+    const { error } = await supabase.auth.verifyOtp({
+      type,
+      token_hash: tokenHash,
+    })
+    if (!error) {
+      return safeRedirect(request, origin, next)
     }
   }
 
   // return the user to an error page with instructions
   return NextResponse.redirect(`${origin}/auth/auth-code-error`)
+}
+
+function safeRedirect(request: Request, origin: string, next: string) {
+  const forwardedHost = request.headers.get('x-forwarded-host') // i.e. vercel.app
+  const isLocalEnv = process.env.NODE_ENV === 'development'
+  
+  if (isLocalEnv) {
+    return NextResponse.redirect(`${origin}${next}`)
+  } else if (forwardedHost) {
+    return NextResponse.redirect(`https://${forwardedHost}${next}`)
+  } else {
+    return NextResponse.redirect(`${origin}${next}`)
+  }
 }
